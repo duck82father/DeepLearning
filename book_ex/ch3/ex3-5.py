719671dfb3444e3de2bfe0470ac13b7dbeb4d104
duck82father <legnaevol84@gmail.com>
2023-11-09 14:02:42 +0900

    2023-11-09

--- book_ex/ch3/ex3-5.py
+++ book_ex/ch3/ex3-5.py
@@ -1,6 +1,7 @@
 from konlpy.tag import Komoran
+import os
 
-komoran = Komoran(userdic='./user_dic.tsv')
+komoran = Komoran(userdic=os.path.join(os.getcwd(),'book_ex', 'ch3', 'user_dic.tsv'))
 text = "우리 챗봇은 엔엘피를 좋아해."
 pos = komoran.pos(text)
 print(pos)
--- book_ex/ch4/create_word2vec_model.py
+++ book_ex/ch4/create_word2vec_model.py
@@ -1,11 +1,11 @@
 from gensim.models import Word2Vec
 from konlpy.tag import Komoran
 import time
-
+import os
 
 # 네이버 영화 리뷰 데이터 읽어옴
 def read_review_data(filename):
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding='UTF8') as f:
         data = [line.split('\t') for line in f.read().splitlines()]
         data = data[1:] # header 제거
     return data
@@ -16,7 +16,7 @@ start = time.time()
 
 # 리뷰 파일 읽어오기
 print('1) 말뭉치 데이터 읽기 시작')
-review_data = read_review_data('./ratings.txt')
+review_data = read_review_data(os.path.join(os.getcwd(),'book_ex', 'ch4', 'ratings.txt'))
 print(len(review_data)) # 리뷰 데이터 전체 개수
 print('1) 말뭉치 데이터 읽기 완료: ', time.time() - start)
 
@@ -28,7 +28,7 @@ print('2) 형태소에서 명사만 추출 완료: ', time.time() - start)
 
 # word2vec 모델 학습
 print('3) word2vec 모델 학습 시작')
-model = Word2Vec(sentences=docs, size=200, window=4, min_count=2, sg=1)
+model = Word2Vec(sentences=docs, vector_size=200, window=4, min_count=2, sg=1)
 print('3) word2vec 모델 학습 완료: ', time.time() - start)
 
 # 모델 저장
